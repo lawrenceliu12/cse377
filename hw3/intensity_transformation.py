e032d4b88358c520e8a99e4ccdc2d563d70e7c2c
Lawrence Liu <lawrence.liu.1@stonybrook.edu>
2023-03-01 21:40:04 -0500

    working?

--- hw3/intensity_transformation.py
+++ hw3/intensity_transformation.py
@@ -1,10 +1,26 @@
-import cv2
-import numpy
+import cv2 #pip install opencv-python
+import numpy #pip install numpy
+
+def piecewise_transformation(negative_image):
+    L = 256
+    rmin = numpy.min(negative_image)
+    rmax = numpy.max(negative_image)
+    smin = 0
+    smax = L - 1
+    slope = (smax - smin) / (rmax - rmin)
+    intercept = smin - slope * rmin
+
+    return numpy.clip((slope * negative_image + intercept), 0, 255)
+
+def power_law_transformation(negative_image):
+    gamma = 0.1
+    power = numpy.power(negative_image, gamma)
+    power = cv2.normalize(power, None, 0, 255, cv2.NORM_MINMAX)
+
+    return numpy.uint8(power)
 
 def log_transformation(negative_image):
-    log_transform = numpy.log(1 + negative_image)
-    log_transform = cv2.normalize(log_transform, None, 0, 255, cv2.NORM_MINMAX)
-
+    log_transform = cv2.normalize(numpy.array(numpy.log(1 + negative_image), dtype=numpy.float32), None, 0, 255, cv2.NORM_MINMAX)
     return numpy.uint8(log_transform)
 
 def negative_image(image):
@@ -15,10 +31,14 @@ def main():
     image = cv2.imread('Mammogram.png', cv2.IMREAD_GRAYSCALE)
     negative = negative_image(image)
     log_transformed = log_transformation(negative)
+    power_law_transformed = power_law_transformation(negative)
+    piecewise_transformed = piecewise_transformation(negative)
 
-    cv2.imshow('Original', image)
-    cv2.imshow('Negative', negative)
-    cv2.imshow('Log Transformed', log_transformed)
+    cv2.imwrite('Negative.png', negative)
+    cv2.imwrite('Log-Transformed.png', log_transformed)
+    cv2.imwrite('Power-Law-Transformed.png', power_law_transformed)
+    cv2.imwrite('Piecewise-Transformed.png', piecewise_transformed)
+
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
