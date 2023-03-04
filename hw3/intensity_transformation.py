e338c93813030d9b80c6f07f589a003a48b48852
Lawrence Liu <lawrence.liu.1@stonybrook.edu>
2023-03-03 23:03:48 -0500

    final

--- hw3/intensity_transformation.py
+++ hw3/intensity_transformation.py
@@ -9,26 +9,25 @@ def piecewise_transformation(negative_image):
     smax = L - 1
     slope = (smax - smin) / (rmax - rmin)
     intercept = smin - slope * rmin
-
     return numpy.clip((slope * negative_image + intercept), 0, 255)
 
 def power_law_transformation(negative_image):
-    gamma = 0.1
-    power = numpy.power(negative_image, gamma)
-    power = cv2.normalize(power, None, 0, 255, cv2.NORM_MINMAX)
+    gamma = 1 #value of gamma
+    power = numpy.power(negative_image, gamma) #power rule of the matrix
+    power = cv2.normalize(power, None, 0, 255, cv2.NORM_MINMAX) #normalize picture
 
-    return numpy.uint8(power)
+    return numpy.uint8(power) #return the matrix in 8 bits.
 
 def log_transformation(negative_image):
-    log_transform = cv2.normalize(numpy.array(numpy.log(1 + negative_image), dtype=numpy.float32), None, 0, 255, cv2.NORM_MINMAX)
-    return numpy.uint8(log_transform)
+    log_transform = cv2.normalize(numpy.array(numpy.log(1 + negative_image), dtype=numpy.float32), None, 0, 255, cv2.NORM_MINMAX) #use the log transformation formula to a new matrix
+    return numpy.uint8(log_transform) #return the matrix in 8 bits.
 
 def negative_image(image):
-    L = 256
-    return (L - 1) - image
+    L = 256 #L
+    return (L - 1) - image #L - 1 - image
 
 def main():
-    image = cv2.imread('Mammogram.png', cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread('Mammogram.png')
     negative = negative_image(image)
     log_transformed = log_transformation(negative)
     power_law_transformed = power_law_transformation(negative)
